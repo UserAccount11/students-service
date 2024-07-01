07e1f00d6257289088ed11679717cc3165c4bd2d
UsserAccount11 <marcocubas13@gmail.com>
2024-07-01 15:39:58 -0500

    Creación de proyecto base.

---
+++ src/main/java/com/tecylab/ms/stundents/app/StudentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tecylab.ms.stundents.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsServiceApplication.class, args);
+	}
+
+}
