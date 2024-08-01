30e098a1ae3a8bec1217a3e4b8109de42a9992e5
UsserAccount11 <marcocubas13@gmail.com>
2024-07-31 20:28:33 -0500

    Agregar datos de prueba.

--- src/main/java/com/tecylab/ms/stundents/app/StudentsServiceApplication.java
+++ src/main/java/com/tecylab/ms/stundents/app/StudentsServiceApplication.java
@@ -1,13 +1,32 @@
 package com.tecylab.ms.stundents.app;
 
+import com.tecylab.ms.stundents.app.insfrastracture.adapters.output.persistence.entity.StudentEntity;
+import com.tecylab.ms.stundents.app.insfrastracture.adapters.output.persistence.repository.StudentJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class StudentsServiceApplication {
+@RequiredArgsConstructor
+public class StudentsServiceApplication implements CommandLineRunner {
+
+	private final StudentJpaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		repository.saveAll(Arrays.asList(
+				new StudentEntity(null, "Carlos", "Campos", 20, "Dirección 1"),
+				new StudentEntity(null, "José", "Lopez", 20, "Dirección 2"),
+				new StudentEntity(null, "Anita", "Ruiz", 20, "Dirección 3"),
+				new StudentEntity(null, "Fatima", "Segovia", 20, "Dirección 4"),
+				new StudentEntity(null, "Fernanda", "Arias", 20, "Dirección 5")));
 	}
 
 }
